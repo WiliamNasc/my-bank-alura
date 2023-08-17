80bd95055b5d0065b1d7b76d535add76d6143439
WiliamNasc <wilnasc990s3@gmail.com>
2023-08-17 20:49:25 -0300

    Adicionando conteúdo aprendido da auÃla

--- src/TesteCriaGerente.java
+++ src/TesteCriaGerente.java
@@ -5,7 +5,7 @@ public class TesteCriaGerente {
         Gerente gerente = new Gerente();
         gerente.setNome("Wiliam");
         gerente.setCpf("445.896.687-54");
-        gerente.setSalario(10000);
+        gerente.setSalario(1000);
         gerente.setSenha(123456);
 
         System.out.println();
@@ -15,5 +15,12 @@ public class TesteCriaGerente {
         System.out.println("Visualizando se a senha é válida");
         System.out.println();
         System.out.println("A senha informada é válida: " + gerente.autentica(123456));
+
+        System.out.println();
+        System.out.println("================================");
+        System.out.println();
+        System.out.println("Salário: " + gerente.getSalario());
+        System.out.println("Percentual bonificação: " + gerente.getPercentualBonificacao() * 100 + "%");
+        System.out.println("Visualizando bonificação: " + gerente.getBonificacao());
     }
 }
