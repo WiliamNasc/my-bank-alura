3ac389b3c7275e0e729ed3de487135b48eb30166
WiliamNasc <wilnasc990s3@gmail.com>
2023-08-23 21:20:53 -0300

    Adicionando prática da aula

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,4 +2,9 @@ public class ContaCorrente extends Conta{
     public ContaCorrente(int agencia, int numero, Cliente cliente) {
         super(agencia, numero, cliente);
     }
+
+    @Override
+    public boolean sacar(double valor) {
+        return super.sacar(valor + 0.2);
+    }
 }
